23f1745cc26d306552a3abdda8f0144892ae7d45
root <root@your.hostname.com>
2026-09-17 10:55:35 +0800

    限流

---
+++ my-project/src/components/HelloWorld1.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello-world-1">
+    hello world 1
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld1'
+}
+</script>
+
+<style scoped>
+.hello-world-1 {
+  font-size: 16px;
+}
+</style>
